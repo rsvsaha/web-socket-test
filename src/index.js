881b5a3685981982b2aaa351448bcaff955f62a1
Rishav Saha <androappdaemon@gmail.com>
2020-07-21 23:51:42 +0530

    Removed React SnapShot

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import {store} from './redux/store';
-import { render } from 'react-snapshot';
-// ReactDOM.render(
-render(
+ReactDOM.render(
 <Provider store={store}>
 <App />
 </Provider>
